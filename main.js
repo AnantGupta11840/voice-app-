dd89258ed6458ad320ef6f43f7d2e053917ed92e
AnantGupta11840 <81515989+AnantGupta11840@users.noreply.github.com>
2021-06-27 17:48:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var Speehrecoginition = window.webkitSpeehReconginition;
+
+var reconginition = new SpeehRecoginition();
+
+var Textbox = document.getElementIdBy (Textbox);
+
+function start (){
+    Textbox.innerhtml= "" ;
+    reconginition.start() ;
+
+}
